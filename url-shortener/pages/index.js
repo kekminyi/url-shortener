7ab6b7a302dcb7a5d81c760a0eabc2d52aae57cf
kek min yi <mkek002@e.ntu.edu.sg>
2022-08-06 18:01:15 +0800

    added text to show shortened URL or error message

--- url-shortener/pages/index.js
+++ url-shortener/pages/index.js
@@ -3,6 +3,7 @@ import {
   Button,
   Center,
   Flex,
+  Text,
   InputGroup,
   FormControl,
   InputRightElement,
@@ -13,13 +14,18 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function Home() {
+  const [error, setError] = useState(false);
+  const [shortenedUrl, setShortenedUrl] = useState("");
   const handleSubmit = async () => {
     console.log(url);
     try {
       await axios.post("/api/generateLink", { url }).then(function (response) {
+        setShortenedUrl(response.data.url);
         console.log(response);
+        setError(false);
       });
     } catch (error) {
+      setError(true);
       console.error(error);
     }
   };
@@ -38,13 +44,12 @@ export default function Home() {
         <Heading p="3%">URL Shortener</Heading>
       </Center>
       <Center>
-        <FormControl isRequired w={"70%"}>
+        <FormControl isRequired w={"70%"} isInvalid={error}>
           <InputGroup>
             <Input
               size="lg"
               placeholder="What is the URL that is being shortened today?"
               value={url}
-              // type="url"
               onChange={handleChange}
             ></Input>
             <InputRightElement width="6.5rem">
@@ -60,6 +65,13 @@ export default function Home() {
             </InputRightElement>
           </InputGroup>
         </FormControl>
+      </Center>
+      <Center>
+        {error ? (
+          <Text p="3%">Invalid URL. Please input a correct URL.</Text>
+        ) : (
+          <Text p="3%">{shortenedUrl}</Text>
+        )}
       </Center>
     </Flex>
   );
